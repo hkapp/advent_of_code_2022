2ec17c3ce2f790017172eb85bf8f77d35651e901
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-01-02 15:09:17 +0100

    Current implementation of day 24 works for the example but runs out of memory on the real one

--- rs/src/astar.rs
+++ rs/src/astar.rs
@@ -16,22 +16,38 @@ pub fn astar<T>(start: T) -> T
     let mut pq = BinaryHeap::new();
     pq.push(start);
 
-    let mut pruned: u64 = 0;
+    let mut late_pruned: u64 = 0;
+    let mut early_pruned: u64 = 0;
     let mut expanded: u64 = 0;
+
+    let mut next_print = 1000;
 
     while let Some(curr_node) = pq.pop() {
         if curr_node.prune(&curr_best) {
-            pruned += 1;
+            late_pruned += 1;
             continue;
         }
-        for next in curr_node.expand() {
-            expanded += 1;
-            pq.push(next);
-        }
+
+        let next_iter = curr_node.expand();
         curr_best = cmp::max(curr_best, curr_node);
+
+        for next in next_iter {
+            if next.prune(&curr_best) {
+                early_pruned += 1;
+            }
+            else {
+                expanded += 1;
+                pq.push(next);
+            }
+        }
+
+        if (expanded + early_pruned) > next_print {
+            println!("astar: expanded {} nodes, pruned {} early and {} late", expanded, early_pruned, late_pruned);
+            next_print *= 10;
+        }
     }
 
-    println!("astar: expanded {} nodes, pruned {}", expanded, pruned);
+    println!("astar: expanded {} nodes, pruned {} early and {} late", expanded, early_pruned, late_pruned);
 
     return curr_best;
 }
--- rs/src/dec24.rs
+++ rs/src/dec24.rs
@@ -5,14 +5,17 @@ use crate::astar::{astar, AStar};
 use std::rc::Rc;
 use multimap::MultiMap;
 use std::vec;
+use std::slice;
+use std::str::FromStr;
+use std::hash::Hash;
 
 type Input<T> = io::BufReader<T>;
 
 pub fn run(file_content: Input<File>) {
-    //let squad = parse(file_content);
+    let (valley, blizzards) = parse(file_content);
 
-    //let res1 = task1(squad.clone());
-    //println!("Task 1: {}", res1);
+    let res1 = task1(&valley, blizzards.clone());
+    println!("Task 1: {}", res1);
     //assert_eq!(res1, 4056);
 
     //let res2 = task2(squad);
@@ -22,28 +25,83 @@ pub fn run(file_content: Input<File>) {
 
 /* Parsing */
 
-//fn parse<T: io::Read>(file_content: Input<T>) -> Squad {
-    //let mut squad = Squad::new();
-    //let mut north = 0;
-    //for line in file_content.lines() {
-		//let mut east = 0;
-		//for c in line.unwrap().chars() {
-			//match c {
-				//'.' => {},
-				//'#' => {
-					//let pos = Pos { north, east };
-					//squad.insert(pos);
-				//},
-				//_   => panic!("Unrecognized input character: {:?}", c),
-			//}
-			///* The columns expand eastward */
-			//east += 1;
-		//}
-		///* The lines expand southward */
-		//north -= 1;
-	//}
-	//return squad;
-//}
+fn parse<T: io::Read>(file_content: Input<T>) -> (Valley, Rc<Blizzards>) {
+    /* Fields for Valley */
+    let north_wall = 0;
+    let mut south_wall = None;
+    let mut east_wall = None;
+    let west_wall = 0;
+    let mut start_pos = None;
+    let mut exit_pos = None;
+
+	let mut blizzards = MultiMap::new();
+
+    let mut north = north_wall;
+    for line in file_content.lines() {
+		let mut east = west_wall;
+		for c in line.unwrap().chars() {
+			let pos = Pos { north, east };
+			match c {
+				'.' => {
+					if north == north_wall {
+						assert!(start_pos.is_none());
+						start_pos = Some(pos);
+					}
+					else if south_wall.is_some() && south_wall.unwrap() == north {
+						assert!(exit_pos.is_none());
+						exit_pos = Some(pos);
+					}
+				},
+				'#' => {
+					if north == north_wall || east == west_wall {
+						/* Do nothing */
+					}
+					else if east == west_wall + 1 {
+						assert!(south_wall.is_none());
+						south_wall = Some(north);
+					}
+					else if east_wall.is_none() {
+						east_wall = Some(east);
+					}
+				},
+				_ => {
+					let dir = c.to_string().parse().unwrap();
+					blizzards.insert(pos, dir);
+				}
+			}
+			/* The columns expand eastward */
+			east += 1;
+		}
+		/* The lines expand southward */
+		north -= 1;
+	}
+
+	let valley = Valley {
+		north_wall,
+		south_wall: south_wall.unwrap(),
+		east_wall:  east_wall.unwrap(),
+		west_wall,
+
+		start_pos: start_pos.unwrap(),
+		exit_pos:  exit_pos.unwrap()
+	};
+
+	(valley, Rc::from(blizzards))
+}
+
+impl FromStr for Dir {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, <Self as FromStr>::Err> {
+		match s {
+			">" => Ok(East),
+			"<" => Ok(West),
+			"^" => Ok(North),
+			"v" => Ok(South),
+			_   => Err(format!("Unrecognized direction: {:?}", s)),
+		}
+	}
+}
 
 /* Coordinate system */
 
@@ -110,10 +168,67 @@ impl Valley {
 	}
 }
 
+/* Blizzards */
+
+type Blizzards = MultiMap<Pos, Dir>;
+
+// For some reason, this is not part of the multimap crate
+fn multi_iter<'a, K: Eq + Hash, V>(multi_map: &'a MultiMap<K, V>) -> MultiIter<'a, K, V> {
+	MultiIter::new(multi_map)
+}
+
+struct MultiIter<'a, K, V> {
+	curr_iter:  Option<(&'a K, slice::Iter<'a, V>)>,
+	vec_iter:   multimap::IterAll<'a, K, Vec<V>>,
+}
+
+impl<'a, K, V> Iterator for MultiIter<'a, K, V> {
+	type Item = (&'a K, &'a V);
+
+	fn next(&mut self) -> Option<Self::Item> {
+		match &mut self.curr_iter {
+			Some((curr_key, value_iter)) => match value_iter.next() {
+				Some(curr_value) => Some((curr_key, curr_value)),
+				None             => {
+					self.next_vec();
+					self.next()
+				}
+			}
+			None => None,
+		}
+	}
+}
+
+impl<'a, K, V> MultiIter<'a, K, V> {
+	fn next_vec(&mut self) {
+		self.curr_iter =
+			self.vec_iter
+				.next()
+				.map(|(k, vec)| (k, vec.iter()))
+	}
+}
+
+impl<'a, K: Eq + std::hash::Hash, V> MultiIter<'a, K, V> {
+	fn new(multi_map: &'a MultiMap<K, V>) -> Self {
+		let mut new = MultiIter {
+			vec_iter:  multi_map.iter_all(),
+			curr_iter: None,
+		};
+		new.next_vec();
+		new
+	}
+}
+
+// For some reason, this is also part of the multimap crate
+fn multi_map_len<K: Eq + Hash, V>(multi_map: &MultiMap<K, V>) -> usize {
+	multi_map.iter_all()
+		.map(|(_, vec)| vec.len())
+		.sum()
+}
+
 /* Expedition */
 
 type Time = u16;
-type Blizzards = MultiMap<Pos, Dir>;
 
 #[derive(Clone)]
 struct Expedition<'a> {
@@ -137,8 +252,15 @@ impl<'a> PartialOrd for Expedition<'a> {
         let vl1 = self.valley as *const Valley;
         let vl2 = other.valley as *const Valley;
         if vl1 == vl2 {
-			/* astar maximizes so we want to minimize */
-			// Start by comparing the time + manhattan distance
+			/* astar maximizes but we want to minimize */
+			// It's always better to escape
+			if self.reached_exit() && !other.reached_exit() {
+				return Some(Ordering::Greater);
+			}
+			if !self.reached_exit() && other.reached_exit() {
+				return Some(Ordering::Less);
+			}
+			// Now compare the time + manhattan distance
 			// Note that this is safe to do even if the expedition is dead
 			match self.potential().cmp(&other.potential()) {
 				Ordering::Greater => return Some(Ordering::Less), /* this is worse */
@@ -182,7 +304,8 @@ impl<'a> AStar for Expedition<'a> {
 	fn expand(&self) -> Self::Expansion {
 		if self.reached_exit() {
 			// This tells astar that we're done
-			return Vec::new().into_iter()
+			println!("\nReached the exit in {} moves", self.time);
+			return Vec::new().into_iter();
 		}
 
 		let mut post_blizzard: Expedition = self.clone();
@@ -195,12 +318,11 @@ impl<'a> AStar for Expedition<'a> {
 				vec![expedition_dies(post_blizzard)]
 			}
 			else {
-				// TODO we want the blizzard to be Rc
-				// TODO increase time in move_to
 				candidate_moves.into_iter()
 					.map(|new_pos| {
 						let mut moved_xp: Expedition = post_blizzard.clone();
 						moved_xp.move_to(new_pos);
+						//println!("{}", moved_xp.potential());
 						moved_xp
 					})
 					.collect()
@@ -209,8 +331,17 @@ impl<'a> AStar for Expedition<'a> {
 	}
 
 	fn prune(&self, curr_best: &Self) -> bool {
+		//println!("Curr best: {}exit {}, potential: {}",
+			//if curr_best.reached_exit() { "" } else { "no " }, curr_best.time, self.potential());
+		//if curr_best.reached_exit()
+			//&& self.potential() >= curr_best.time
+		//{
+			//print!("-");
+		//}
 		// We want to prune if the potential is higher than the current best time
-		self.potential() >= curr_best.time
+		// Only if the curr_best reached the end though
+		curr_best.reached_exit()
+		&& self.potential() >= curr_best.time
 	}
 
 }
@@ -254,11 +385,35 @@ impl<'a> Expedition<'a> {
 	}
 
 	fn move_blizzards(&mut self) {
-		let new_blizzards =
-			self.blizzards
-				.iter()
-				.map(|(pos, dir)| (pos.move_in_dir(*dir), *dir))
+		let move_one_blizzard = |curr_pos: Pos, dir| {
+			let new_pos = curr_pos.move_in_dir(dir);
+			if self.valley.is_valid_pos(new_pos) {
+				new_pos
+			}
+			else {
+				// Wrap around
+				/*  +--^--v--+
+				 *  >        >
+				 *  |        |
+				 *  <        <
+				 *  +--^--v--+
+				 */
+				match dir {
+					North => Pos { north: self.valley.south_wall + 1, east: new_pos.east },
+					South => Pos { north: self.valley.north_wall - 1, east: new_pos.east },
+					East  => Pos { north: new_pos.north, east: self.valley.west_wall + 1 },
+					West  => Pos { north: new_pos.north, east: self.valley.east_wall - 1 },
+				}
+			}
+		};
+
+		let prev_size = multi_map_len(&self.blizzards);
+		let new_blizzards: Blizzards =
+			multi_iter(&self.blizzards)
+				.map(|(pos, dir)| (move_one_blizzard(*pos, *dir), *dir))
 				.collect();
+		assert_eq!(multi_map_len(&new_blizzards), prev_size, "new_blizzards: {:?}", new_blizzards);
+
 		self.blizzards = Rc::new(new_blizzards);
 	}
 
@@ -277,6 +432,23 @@ impl<'a> Expedition<'a> {
 			self.time + self.curr_pos.manhattan_dist(self.valley.exit_pos) as u16
 		}
 	}
+
+	fn new(valley: &'a Valley, blizzards: Rc<Blizzards>) -> Self {
+		Expedition {
+			curr_pos:  valley.start_pos,
+			time:      0,
+			blizzards,
+			valley,
+		}
+	}
+}
+
+/* Task 1 */
+
+fn task1(valley: &Valley, init_blizzards: Rc<Blizzards>) -> Time {
+	let start_xp = Expedition::new(valley, init_blizzards.clone());
+	let final_xp = astar(start_xp);
+	final_xp.time
 }
 
 /* Unit tests */
@@ -285,167 +457,183 @@ impl<'a> Expedition<'a> {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "....#..
-..###.#
-#...#.#
-.#...##
-#.###..
-##.#.##
-.#..#..";
+    const EXAMPLE: &str = "#.######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#";
 
     lazy_static! {
-        static ref EXAMPLE_SQUAD: Squad = parse_str(EXAMPLE);
+        static ref EXAMPLE_VALLEY:    Valley = parse_str(EXAMPLE).0;
+        static ref EXAMPLE_BLIZZARDS: Blizzards = Rc::try_unwrap(parse_str(EXAMPLE).1).unwrap();
     }
 
-    fn parse_str(s: &str) -> Squad {
+    fn parse_str(s: &str) -> (Valley, Rc<Blizzards>) {
 		parse(io::BufReader::new(s.as_bytes()))
+	}
+
+	fn example_blizzards() -> Rc<Blizzards> {
+		Rc::from(EXAMPLE_BLIZZARDS.clone())
 	}
 
     #[test]
     fn validate_task1() {
-        assert_eq!(task1(EXAMPLE_SQUAD.clone()), 110);
+        assert_eq!(task1(&EXAMPLE_VALLEY, example_blizzards()), 18);
     }
+
+	fn assert_fmt<T: Eq + std::fmt::Debug + std::fmt::Display>(left: T, right: T) {
+		assert_eq!(left, right, "\nleft:\n{}\nright:\n{}\n", left, right);
+	}
 
     #[test]
-    fn validate_task2() {
-        assert_eq!(task2(EXAMPLE_SQUAD.clone()), 20);
-    }
+    fn validate_task1_steps() {
+		let mut xp = Expedition::new(&EXAMPLE_VALLEY, example_blizzards());
+		let expected = "#E######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#";
+		assert_fmt::<&str>(&show_expedition(&xp), expected);
+
+		/* Minute 1, move down: */
+		xp.move_to(xp.curr_pos.move_in_dir(South));
+		xp.move_blizzards();
+		let expected = "#.######
+#E>3.<.#
+#<..<<.#
+#>2.22.#
+#>v..^<#
+######.#";
+		assert_fmt::<&str>(&show_expedition(&xp), expected);
+
+		/* Minute 2, move down: */
+		xp.move_to(xp.curr_pos.move_in_dir(South));
+		xp.move_blizzards();
+		let expected = "#.######
+#.2>2..#
+#E^22^<#
+#.>2.^>#
+#.>..<.#
+######.#";
+		assert_fmt::<&str>(&show_expedition(&xp), expected);
+	}
+
+	impl Into<char> for Dir {
+		fn into(self) -> char {
+			match self {
+				North => '^',
+				South => 'v',
+				East  => '>',
+				West  => '<',
+			}
+		}
+	}
+
+	fn show_expedition(xp: &Expedition) -> String {
+		let mut buf = String::new();
+		for north in (xp.valley.south_wall..(xp.valley.north_wall+1)).rev() {
+			for east in xp.valley.west_wall..(xp.valley.east_wall+1) {
+				let pos = Pos { north, east };
+				let c =
+					if xp.valley.is_valid_pos(pos) {
+						if xp.curr_pos == pos {
+							'E'
+						}
+						else {
+							let empty_vec = Vec::new();
+							let blizzards_here = xp.blizzards.get_vec(&pos).unwrap_or(&empty_vec);
+							match blizzards_here.len() {
+								0 => '.',
+								1 => (*blizzards_here.get(0).unwrap()).into(),
+								n => {
+									assert!(n < 10);
+									format!("{}", n).chars().next().unwrap()
+								}
+							}
+						}
+					}
+					else {
+						// This must be a wall
+						'#'
+					};
+				buf.push(c);
+			}
+			if north > xp.valley.south_wall {
+				buf.push('\n');
+			}
+		}
+		return buf;
+	}
 
     #[test]
-    fn test_small_steps() {
-        let mut squad = parse_str(
-".....
-..##.
-..#..
-.....
-..##.
-.....");
-        let mut rule_book = init_rule_book();
+    fn test_blizzard_wraparound() {
+		let simpler_example: &str = "#.######
+#....^>#
+#......#
+#<.....#
+#....v.#
+######.#";
+		let (valley, blizzards) = parse_str(simpler_example);
+		let mut xp = Expedition::new(&valley, blizzards);
+		let expected = "#E######
+#....^>#
+#......#
+#<.....#
+#....v.#
+######.#";
+		assert_fmt::<&str>(&show_expedition(&xp), expected);
 
-        elf_round(&mut squad, &mut rule_book);
-        assert_eq!(squad, parse_str(
-"..##.
-.....
-..#..
-...#.
-..#..
-....."));
-
-        elf_round(&mut squad, &mut rule_book);
-        assert_eq!(squad, parse_str(
-".....
-..##.
-.#...
-....#
-.....
-..#.."));
-
-        elf_round(&mut squad, &mut rule_book);
-        assert_eq!(squad, parse_str(
-"..#..
-....#
-#....
-....#
-.....
-..#.."));
-    }
-
-    #[test]
-    fn test_example_steps() {
-		// == Initial State ==
-        let mut squad = parse_str(
-"..............
-..............
-.......#......
-.....###.#....
-...#...#.#....
-....#...##....
-...#.###......
-...##.#.##....
-....#..#......
-..............
-..............
-..............");
-        let mut rule_book = init_rule_book();
-
-		// == End of Round 1 ==
-        elf_round(&mut squad, &mut rule_book);
-        assert_eq!(squad, parse_str(
-"..............
-.......#......
-.....#...#....
-...#..#.#.....
-.......#..#...
-....#.#.##....
-..#..#.#......
-..#.#.#.##....
-..............
-....#..#......
-..............
-.............."));
-
-        // == End of Round 2 ==
-        elf_round(&mut squad, &mut rule_book);
-        show_squad_within(&squad, &Rect { north_edge: 1, south_edge: -10, east_edge: 12, west_edge: 1 });
-        assert_eq!(squad, parse_str(
-"..............
-.......#......
-....#.....#...
-...#..#.#.....
-.......#...#..
-...#..#.#.....
-.#...#.#.#....
-..............
-..#.#.#.##....
-....#..#......
-..............
-.............."));
-
-        // == End of Round 3 ==
-        elf_round(&mut squad, &mut rule_book);
-        assert_eq!(squad, parse_str(
-"..............
-.......#......
-.....#....#...
-..#..#...#....
-.......#...#..
-...#..#.#.....
-.#..#.....#...
-.......##.....
-..##.#....#...
-...#..........
-.......#......
-.............."));
-    }
+		xp.move_blizzards();
+		let expected = "#E######
+#>...v.#
+#......#
+#.....<#
+#....^.#
+######.#";
+		assert_fmt::<&str>(&show_expedition(&xp), expected);
+	}
 
 	#[test]
-	fn test_rule_book() {
-		let mut rule_book = init_rule_book();
+	fn test_multi_iter() {
+		let mut multi_map = MultiMap::new();
+		multi_map.insert(2 as u8, 2 as u8);
+		multi_map.insert(3 as u8, 3 as u8);
+		multi_map.insert(4 as u8, 4 as u8);
+		multi_map.insert(4 as u8, 2 as u8);
 
-		// Round 1
-		let mut iter = rule_book.iter();
-		assert_eq!(iter.next(), Some(&North));
-		assert_eq!(iter.next(), Some(&South));
-		assert_eq!(iter.next(), Some(&West));
-		assert_eq!(iter.next(), Some(&East));
-		assert_eq!(iter.next(), None);
-
-		// Round 2
-		rule_book.advance(1);
-		let mut iter = rule_book.iter();
-		assert_eq!(iter.next(), Some(&South));
-		assert_eq!(iter.next(), Some(&West));
-		assert_eq!(iter.next(), Some(&East));
-		assert_eq!(iter.next(), Some(&North));
-		assert_eq!(iter.next(), None);
-
-		// Round 3
-		rule_book.advance(1);
-		let mut iter = rule_book.iter();
-		assert_eq!(iter.next(), Some(&West));
-		assert_eq!(iter.next(), Some(&East));
-		assert_eq!(iter.next(), Some(&North));
-		assert_eq!(iter.next(), Some(&South));
-		assert_eq!(iter.next(), None);
+		assert_eq!(multi_map.iter().count(), 3);
+		assert_eq!(multi_map.iter_all().count(), 3);
+		assert_eq!(multi_iter(&multi_map).count(), 4);
 	}
+
+	#[test]
+	fn test_multi_map_collect() {
+		let mut multi_map = MultiMap::new();
+		multi_map.insert(2 as u8, 2 as u8);
+		multi_map.insert(3 as u8, 3 as u8);
+		multi_map.insert(4 as u8, 4 as u8);
+		multi_map.insert(4 as u8, 2 as u8);
+
+		let new_map: MultiMap<u8, u8> =
+			multi_iter(&multi_map)
+				.map(|(k, v)| (*k, *v))
+				.collect();
+
+		assert_eq!(new_map, multi_map);
+	}
+
+	#[test]
+	fn test_multi_map_len() {
+		let mut multi_map = MultiMap::new();
+		multi_map.insert(2 as u8, 2 as u8);
+		multi_map.insert(3 as u8, 3 as u8);
+		multi_map.insert(4 as u8, 4 as u8);
+		multi_map.insert(4 as u8, 2 as u8);
+
+		assert_eq!(multi_map.len(), 3);
+		assert_eq!(multi_map_len(&multi_map), 4);
+	}
+
 }
