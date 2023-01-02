3b89bb54dc6a89c49544a5d6099495098670ad87
Hugo Kapp <hugo.kapp6@hotmail.fr>
2023-01-02 10:07:16 +0100

    Complete day 23

--- rs/src/dec23.rs
+++ rs/src/dec23.rs
@@ -9,13 +9,13 @@ type Input<T> = io::BufReader<T>;
 pub fn run(file_content: Input<File>) {
     let squad = parse(file_content);
 
-    let res1 = task1(squad);
+    let res1 = task1(squad.clone());
     println!("Task 1: {}", res1);
-    //assert_eq!(res1, 88226);
+    assert_eq!(res1, 4056);
 
-    //let res2 = task2(&map_proj, &moves);
-    //println!("Task 2: {}", res2);
-    //assert_eq!(res2, 57305);
+    let res2 = task2(squad);
+    println!("Task 2: {}", res2);
+    assert_eq!(res2, 999);
 }
 
 /* Parsing */
@@ -53,7 +53,7 @@ struct Pos {
 	east:  Coord,
 }
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum Dir {
 	North, South, East, West
 }
@@ -211,23 +211,29 @@ fn phase1(squad: &Squad, rule_book: &RuleBook) -> Phase1 {
 
 /* Phase 2 */
 
-fn phase2(squad: &mut Squad, phase1: &Phase1) {
+type Count = u16;
+
+fn phase2(squad: &mut Squad, phase1: &Phase1) -> Count {
+	let mut move_count = 0;
 	for prop in &phase1.propositions {
 		let (prev_pos, new_pos) = *prop;
 		let prop_count = *phase1.prop_count.get(&new_pos).unwrap();
 		if prop_count == 1 {
 			squad.remove(&prev_pos);
 			squad.insert(new_pos);
+			move_count += 1;
 		}
 	}
+	return move_count;
 }
 
 /* Round */
 
-fn elf_round(squad: &mut Squad, rule_book: &mut RuleBook) {
+fn elf_round(squad: &mut Squad, rule_book: &mut RuleBook) -> Count {
 	let p1 = phase1(squad, rule_book);
-	phase2(squad, &p1);
+	let move_count = phase2(squad, &p1);
 	rule_book.advance(1);
+	return move_count;
 }
 
 /* Rect */
@@ -262,6 +268,7 @@ impl Rect {
 		// Note: if south_edge == north_edge, we still count height=1
 		let height = self.north_edge - self.south_edge + 1;
 		let width  = self.east_edge - self.west_edge + 1;
+		println!("height: {}, width: {}", height, width);
 		height * width
 	}
 }
@@ -269,7 +276,7 @@ impl Rect {
 /* Task 1 */
 
 fn init_rule_book() -> RuleBook {
-	RuleBook::from(vec![North, South, East, West])
+	RuleBook::from(vec![North, South, West, East])
 }
 
 fn task1(mut squad: Squad) -> Coord {
@@ -279,7 +286,37 @@ fn task1(mut squad: Squad) -> Coord {
 	}
 
 	let rect = enclosing_rect(&squad);
+	show_squad_within(&squad, &rect);
 	rect.area() - squad.len() as Coord
+}
+
+fn show_squad_within(squad: &Squad, rect: &Rect) {
+	for north in (rect.south_edge..rect.north_edge).rev() {
+		for east in rect.west_edge..rect.east_edge {
+			let pos = Pos { north, east };
+			if squad.contains(&pos) {
+				print!("#");
+			}
+			else {
+				print!(".");
+			}
+		}
+		println!();
+	}
+}
+
+/* Task 2 */
+
+fn task2(mut squad: Squad) -> u32 {
+	let mut rule_book = init_rule_book();
+	let mut prev_moved = 1;
+	let mut round_count = 0;
+	while prev_moved > 0 {
+		prev_moved = elf_round(&mut squad, &mut rule_book);
+		round_count += 1;
+	}
+
+	return round_count;
 }
 
 /* Unit tests */
@@ -288,275 +325,167 @@ fn task1(mut squad: Squad) -> Coord {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#...
-..#....#....
-..........#.
-        ...#....
-        .....#..
-        .#......
-        ......#.
-
-10R5L5R10L4R5L5";
+    const EXAMPLE: &str = "....#..
+..###.#
+#...#.#
+.#...##
+#.###..
+##.#.##
+.#..#..";
 
     lazy_static! {
-        static ref EXAMPLE_PLANET: MapProjection =
-			parse(io::BufReader::new(EXAMPLE.as_bytes())).0;
-        static ref EXAMPLE_MOVES: Vec<Move> =
-			parse(io::BufReader::new(EXAMPLE.as_bytes())).1;
-
-		static ref EXAMPLE_CUBE: Cube = build_test_cube();
+        static ref EXAMPLE_SQUAD: Squad = parse_str(EXAMPLE);
     }
-	const A: Pos = Pos { row: 6, column: 12 };
-	const B: Pos = Pos { row: 9, column: 15 };
-	const C: Pos = Pos { row:12, column: 11 };
-	const D: Pos = Pos { row: 8, column:  2 };
 
-	fn build_test_cube() -> Cube {
-		/*      +---+
-			    | 1 |
-		+---+---+---+
-		| 2 | 3 | 4 |
-		+---+---+---+---+
-		        | 5 | 6 |
-		        +---+---+
-		*/
-		let layout =
-			[
-				vec![None,    None,    Some(1), None],
-				vec![Some(2), Some(3), Some(4), None],
-				vec![None,    None,    Some(5), Some(6)],
-			];
-
-		Cube {
-			faces: build_faces_from_layout(4, &layout),
-			edges: build_test_edges()
-		}
-	}
-
-	/*
-			 +-------------+
-			 :             :
-			 : +---------+ :
-			 : :         : :
-			 : :       +-^-V-+
-			 : :   +--->     >-----------+
-			 : :   :   |  1  |           :
-			 : :   : +-<     <--------+  :
-		   +-^-V-+-^-V-+-^-V-+        :  :
-	 +----->     >     >     >---+    :  :
-	 :     |  2  |  3  |  4  |   :    :  :
-	 :  +--<     <     <     <-+ :    :  :
-	 :  :  +-^-V-+-^-V-+-^-V-+-^-V-+  :  :
-	 :  :    : :   : +->     >     >--+  :
-	 :  :    : :   :   |  5  |  6  |     :
-	 :  :    : :   +---<     <     <-----+
-	 :  :    : :       +-^-V-+-^-V-+
-	 :  :    : :         : :   : :
-	 :  :    : +---------+ :   : :
-	 :  :    :             :   : :
-	 :  :    +-------------+   : :
-	 :  :                      : :
-	 :  +----------------------+ :
-	 :                           :
-	 +---------------------------+
-	*/
-
-	/*
-	 When    |      Up        |      Down      |      Left      |      Right     |
-	 exiting | Go to | Rotate | Go to | Rotate | Go to | Rotate | Go to | Rotate |
-	---------+----------------+----------------+----------------+----------------+
-	   1     |   2       x2   |   4       No   |   3       KW   |   6       x2   |
-	   2     |   1       x2   |   5       x2   |   6       CW   |   3       No   |
-	   3     |   1       CW   |   5       KW   |   2       No   |   4       No   |
-	   4     |   1       No   |   5       No   |   3       No   |   6       CW   |
-	   5     |   4       No   |   2       x2   |   3       CW   |   6       No   |
-	   6     |   4       KW   |   2       KW   |   5       No   |   1       x2   |
-
-	Legend:
-	  No: don't rotate
-	  CW: clockwise
-	  KW: counter-clockwise
-	  2x: full turn
-
-	  Note that rotating requires changing the facing direction
-	  but also the coordinates when entering the new face.
-	*/
-	fn build_test_edges() -> CubeEdges {
-		use Rotation::*;
-		HashMap::from(
-			[
-				// 1     |   2       x2   |   4       No   |   3       KW   |   6       x2   |
-				((1, Up),    Edge { destination: 2, rotation: vec![Clockwise, Clockwise] }),
-				((1, Down),  Edge { destination: 4, rotation: vec![] }),
-				((1, Left),  Edge { destination: 3, rotation: vec![CounterClockwise] }),
-				((1, Right), Edge { destination: 6, rotation: vec![Clockwise, Clockwise] }),
-				// 2     |   1       x2   |   5       x2   |   6       CW   |   3       No   |
-				((2, Up),    Edge { destination: 1, rotation: vec![Clockwise, Clockwise] }),
-				((2, Down),  Edge { destination: 5, rotation: vec![Clockwise, Clockwise] }),
-				((2, Left),  Edge { destination: 6, rotation: vec![Clockwise] }),
-				((2, Right), Edge { destination: 3, rotation: vec![] }),
-				// 3     |   1       CW   |   5       KW   |   2       No   |   4       No   |
-				((3, Up),    Edge { destination: 1, rotation: vec![Clockwise] }),
-				((3, Down),  Edge { destination: 5, rotation: vec![CounterClockwise] }),
-				((3, Left),  Edge { destination: 2, rotation: vec![] }),
-				((3, Right), Edge { destination: 4, rotation: vec![] }),
-				// 4     |   1       No   |   5       No   |   3       No   |   6       CW   |
-				((4, Up),    Edge { destination: 1, rotation: vec![] }),
-				((4, Down),  Edge { destination: 5, rotation: vec![] }),
-				((4, Left),  Edge { destination: 3, rotation: vec![] }),
-				((4, Right), Edge { destination: 6, rotation: vec![Clockwise] }),
-				// 5     |   4       No   |   2       x2   |   3       CW   |   6       No   |
-				((5, Up),    Edge { destination: 4, rotation: vec![] }),
-				((5, Down),  Edge { destination: 2, rotation: vec![Clockwise, Clockwise] }),
-				((5, Left),  Edge { destination: 3, rotation: vec![Clockwise] }),
-				((5, Right), Edge { destination: 6, rotation: vec![] }),
-				// 6     |   4       KW   |   2       KW   |   5       No   |   1       x2   |
-				((6, Up),    Edge { destination: 4, rotation: vec![CounterClockwise] }),
-				((6, Down),  Edge { destination: 2, rotation: vec![CounterClockwise] }),
-				((6, Left),  Edge { destination: 5, rotation: vec![] }),
-				((6, Right), Edge { destination: 1, rotation: vec![Clockwise, Clockwise] }),
-			]
-		)
+    fn parse_str(s: &str) -> Squad {
+		parse(io::BufReader::new(s.as_bytes()))
 	}
 
     #[test]
     fn validate_task1() {
-        assert_eq!(task1(&EXAMPLE_PLANET, &EXAMPLE_MOVES), 6032);
+        assert_eq!(task1(EXAMPLE_SQUAD.clone()), 110);
     }
 
     #[test]
     fn validate_task2() {
-        assert_eq!(task2_param(&EXAMPLE_PLANET, build_test_cube(), &EXAMPLE_MOVES), 5031);
+        assert_eq!(task2(EXAMPLE_SQUAD.clone()), 20);
     }
 
     #[test]
-    fn test_cube_physics() {
-		/* Example:
-        ...#
-        .#..
-        #...
-        ....
-...#.......#
-........#..A>-+
-..#....#....  :
-.D........#.  V
- ^      ...#..B.
- :      .....#..
- :      .#......
- :      ..C...#.
- :        V
- +--------+
-        */
+    fn test_small_steps() {
+        let mut squad = parse_str(
+".....
+..##.
+..#..
+.....
+..##.
+.....");
+        let mut rule_book = init_rule_book();
 
-        assert_eq!(
-			EXAMPLE_CUBE.wrap_around(A, Right),
-			(B, Down));
-        assert_eq!(
-			EXAMPLE_CUBE.wrap_around(C, Down),
-			(D, Up));
+        elf_round(&mut squad, &mut rule_book);
+        assert_eq!(squad, parse_str(
+"..##.
+.....
+..#..
+...#.
+..#..
+....."));
+
+        elf_round(&mut squad, &mut rule_book);
+        assert_eq!(squad, parse_str(
+".....
+..##.
+.#...
+....#
+.....
+..#.."));
+
+        elf_round(&mut squad, &mut rule_book);
+        assert_eq!(squad, parse_str(
+"..#..
+....#
+#....
+....#
+.....
+..#.."));
     }
 
     #[test]
-    fn test_rotate_within_face() {
-		/* Clockwise:
-           .2.#
-           ...3
-           1#..
-           ..C.
+    fn test_example_steps() {
+		// == Initial State ==
+        let mut squad = parse_str(
+"..............
+..............
+.......#......
+.....###.#....
+...#...#.#....
+....#...##....
+...#.###......
+...##.#.##....
+....#..#......
+..............
+..............
+..............");
+        let mut rule_book = init_rule_book();
 
-           Counter-clockwise is the opposite
-        */
-        let p1 = Pos {
-			column: C.column - 2,
-			row:    C.row - 1,
-		};
-        let p2 = Pos {
-			column: C.column - 1,
-			row:    p1.row - 2,
-		};
-        let p3 = Pos {
-			column: C.column + 1,
-			row:    p2.row + 1,
-		};
+		// == End of Round 1 ==
+        elf_round(&mut squad, &mut rule_book);
+        assert_eq!(squad, parse_str(
+"..............
+.......#......
+.....#...#....
+...#..#.#.....
+.......#..#...
+....#.#.##....
+..#..#.#......
+..#.#.#.##....
+..............
+....#..#......
+..............
+.............."));
 
-		let face = EXAMPLE_CUBE.get_face(identify_face(&EXAMPLE_CUBE, C).unwrap());
-		use Rotation::*;
+        // == End of Round 2 ==
+        elf_round(&mut squad, &mut rule_book);
+        show_squad_within(&squad, &Rect { north_edge: 1, south_edge: -10, east_edge: 12, west_edge: 1 });
+        assert_eq!(squad, parse_str(
+"..............
+.......#......
+....#.....#...
+...#..#.#.....
+.......#...#..
+...#..#.#.....
+.#...#.#.#....
+..............
+..#.#.#.##....
+....#..#......
+..............
+.............."));
 
-        assert_eq!(
-			rotate_within_face(face, C, Clockwise),
-			p1);
-        assert_eq!(
-			rotate_within_face(face, p1, Clockwise),
-			p2);
-        assert_eq!(
-			rotate_within_face(face, p2, Clockwise),
-			p3);
-        assert_eq!(
-			rotate_within_face(face, p3, Clockwise),
-			C);
-
-        assert_eq!(
-			rotate_within_face(face, C, CounterClockwise),
-			p3);
-        assert_eq!(
-			rotate_within_face(face, p3, CounterClockwise),
-			p2);
-        assert_eq!(
-			rotate_within_face(face, p2, CounterClockwise),
-			p1);
-        assert_eq!(
-			rotate_within_face(face, p1, CounterClockwise),
-			C);
+        // == End of Round 3 ==
+        elf_round(&mut squad, &mut rule_book);
+        assert_eq!(squad, parse_str(
+"..............
+.......#......
+.....#....#...
+..#..#...#....
+.......#...#..
+...#..#.#.....
+.#..#.....#...
+.......##.....
+..##.#....#...
+...#..........
+.......#......
+.............."));
     }
 
-    fn test_full_circle(cube: Cube, start_pos: Pos) {
-		let mut map_proj = HashMap::new();
-		for face in cube.faces.iter() {
-			for row in face.top_row..(face.bot_row+1) {
-				for column in face.left_col..(face.right_col+1) {
-					map_proj.insert(Pos {row, column}, Tile::Open);
-				}
-			}
-		}
-		let recurr_time = 50*4;
+	#[test]
+	fn test_rule_book() {
+		let mut rule_book = init_rule_book();
 
-		let mut astronaut = Astronaut {
-			planet: Planet {
-				shape_3d:   cube,
-				terrain_2d: &map_proj,
-			},
-			curr_dir: Down,
-			curr_pos: start_pos
-		};
-		astronaut.walk(recurr_time);
-		assert_eq!(astronaut.curr_pos, start_pos);
+		// Round 1
+		let mut iter = rule_book.iter();
+		assert_eq!(iter.next(), Some(&North));
+		assert_eq!(iter.next(), Some(&South));
+		assert_eq!(iter.next(), Some(&West));
+		assert_eq!(iter.next(), Some(&East));
+		assert_eq!(iter.next(), None);
 
-		astronaut.curr_dir = Left;
-		astronaut.walk(recurr_time);
-		assert_eq!(astronaut.curr_pos, start_pos);
+		// Round 2
+		rule_book.advance(1);
+		let mut iter = rule_book.iter();
+		assert_eq!(iter.next(), Some(&South));
+		assert_eq!(iter.next(), Some(&West));
+		assert_eq!(iter.next(), Some(&East));
+		assert_eq!(iter.next(), Some(&North));
+		assert_eq!(iter.next(), None);
 
-		astronaut.curr_dir = Right;
-		astronaut.walk(recurr_time);
-		assert_eq!(astronaut.curr_pos, start_pos);
-
-		astronaut.curr_dir = Up;
-		astronaut.walk(recurr_time);
-		assert_eq!(astronaut.curr_pos, start_pos);
+		// Round 3
+		rule_book.advance(1);
+		let mut iter = rule_book.iter();
+		assert_eq!(iter.next(), Some(&West));
+		assert_eq!(iter.next(), Some(&East));
+		assert_eq!(iter.next(), Some(&North));
+		assert_eq!(iter.next(), Some(&South));
+		assert_eq!(iter.next(), None);
 	}
-
-    #[test]
-    fn test_real_cube() {
-		let cube = Cube::new();
-		let start_pos = Pos {
-			row:    cube.faces[0].top_row + 1,
-			column: cube.faces[0].left_col + 1,
-		};
-		test_full_circle(cube, start_pos);
-    }
-
 }
